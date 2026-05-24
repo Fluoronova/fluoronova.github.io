62f87f1bbc95949b79c1353b51beb344e6372737
Twc <twc.001.official@gmail.com>
2026-05-24 17:02:12 +0100

    Cleaned up dividers, lazy load of YouTube embeds alongside youtube-nocookie, and creation of home.js

---
+++ assets/js/home.js
@@ -0,0 +1,19 @@
+const latestVideos = document.getElementById("latestVideos");
+const videoIds = [
+    "QpHAebuGJl0",
+    "QpHAebuGJl0",
+    "QpHAebuGJl0",
+];
+
+latestVideos.innerHTML = "";
+
+for (const id of videoIds) {
+    const videoEmbed = document.createElement("div");
+    videoEmbed.className = "video-card";
+
+    videoEmbed.innerHTML = `
+        <iframe src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video" loading="lazy" referrerpolicy="strict-origin-when-cross-origin" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>
+    `;
+
+    latestVideos.appendChild(videoEmbed);
+}
