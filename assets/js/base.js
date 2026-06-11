6f9b61721ceaf03b86c33ce232ac8a4a5cccb8c2
Twc <twc.001.official@gmail.com>
2026-06-11 21:19:54 +0100

    Preparations for v1.0.0

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,3 +1,9 @@
+// var loadingScreen = document.querySelector(".loading-screen");
+// loadingScreen.style.opacity = 1
+// window.addEventListener('load', function() {
+//   loadingScreen.style.opacity = 0
+// })
+
 document.addEventListener('DOMContentLoaded', function () {
   const versionLabel = document.querySelector('.version-label');
   if (!versionLabel) return;
